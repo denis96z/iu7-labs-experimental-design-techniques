df5d63f6d304205e9f959144a9f2c6e691cf978f
denis96z <zinoviev.denis@yandex.ru>
2018-05-05 22:46:48 +0300

    Time generators implemented. Model interface added.

---
+++ Lab01/Lab01/Model.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Lab01
+{
+    public interface ITimeGenerator
+    {
+        double GetNextValue();
+    }
+
+    public class RandTimeGenerator : ITimeGenerator
+    {
+        private readonly IRandomizer _randomizer;
+
+        public RandTimeGenerator(IRandomizer randomizer)
+        {
+            _randomizer = randomizer;
+        }
+
+        public double GetNextValue()
+        {
+            return _randomizer.GetRandom();
+        }
+    }
+
+    public class ConstTimeGenerator : ITimeGenerator
+    {
+        private readonly int _time;
+
+        public ConstTimeGenerator(int time)
+        {
+            _time = time;
+        }
+
+        public double GetNextValue()
+        {
+            return _time;
+        }
+    }
+
+    public class Model
+    {
+        private readonly ITimeGenerator _reqTimeGenerator;
+        private readonly ITimeGenerator _servTimeGenerator;
+
+        public Model(ITimeGenerator generatorTimeGenerator,
+            ITimeGenerator serviceTimeGenerator)
+        {
+            _reqTimeGenerator = generatorTimeGenerator ?? throw new ArgumentNullException();
+            _servTimeGenerator = serviceTimeGenerator ?? throw new ArgumentNullException();
+        }
+
+        public double CountTime(int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
