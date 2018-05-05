4daa8801997dcc6ddb61079c49d0a95abf0e486a
denis96z <zinoviev.denis@yandex.ru>
2018-05-05 15:49:52 +0300

    Lab01 project added.

---
+++ Lab01/Lab01/MainForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Lab01
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
