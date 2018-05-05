368e07342b7722cdc78aa372ea73bc37780ed9dc
denis96z <zinoviev.denis@yandex.ru>
2018-05-05 23:15:48 +0300

    Calculator interface added.

---
+++ Lab01/Lab01/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab01
+{
+    public struct CalculationResults
+    {
+        public double RealTime { get; set; }
+        public (double, double, double, double) Coeffs { get; set; }
+        public (double, double) Time { get; set; }
+    }
+
+    public class Calculator
+    {
+        public CalculationResults Calculate(IRandomizer reqRandomizer,
+            IRandomizer servRandomizer, int n)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
